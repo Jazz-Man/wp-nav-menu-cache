c45531f755eebe2fb1911fc32ed5bf4ffa3339df
Vasyl Sokolyk <vsokolyk@gmail.com>
2022-09-30 19:35:15 +0300

    style(plugin): fix code style

--- wp-nav-menu-cache.php
+++ wp-nav-menu-cache.php
@@ -8,13 +8,13 @@
  * Requires at least:   5.2
  * Requires PHP:        7.4
  * License:             MIT
- * Update URI:          https://github.com/Jazz-Man/wp-nav-menu-cache
+ * Update URI:          https://github.com/Jazz-Man/wp-nav-menu-cache.
  */
 
 use JazzMan\WpNavMenuCache\NavMenuCache;
 
-if ( function_exists('app_autoload_classes') && class_exists(NavMenuCache::class)) {
-	app_autoload_classes([
-		NavMenuCache::class,
-	]);
+if (function_exists('app_autoload_classes') && class_exists(NavMenuCache::class)) {
+    app_autoload_classes([
+        NavMenuCache::class,
+    ]);
 }
